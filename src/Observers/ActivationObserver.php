58e0541d0e75dc470d8dae723b46597b2f8e16d4
Security Theater <moeteam2016@gmail.com>
2018-01-28 17:51:54 +0200

    Set activation tokens automatically

--- src/Observers/ActivationObserver.php
+++ src/Observers/ActivationObserver.php
@@ -15,11 +15,15 @@ class ActivationObserver
     {
         $activation->completed = (bool) $activation->completed;
         $activation->updated_at = null;
+        $activation->token = str_random(32);
+
     }
 
     public function updating(Model $activation)
     {
         $activation->completed = (bool) $activation->completed;
         $activation->updated_at = date('Y-m-d H:i:s');
+        $activation->token = str_random(32);
+
     }
 }
