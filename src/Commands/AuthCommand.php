1fdccf479976dd1e99d432c8eb9c5d3c9890b041
Security Theater <moeteam2016@gmail.com>
2018-02-01 07:17:01 +0200

    fixed copying auth files

--- src/Commands/AuthCommand.php
+++ src/Commands/AuthCommand.php
@@ -41,8 +41,8 @@ class AuthCommand extends Command
     {
         $this->filesystem->copyDirectory('vendor/sectheater/laravel-jarvis/src/Http/Controllers/Auth', 'app/Http/Controllers/Auth');
         $this->filesystem->copyDirectory('vendor/sectheater/laravel-jarvis/src/publishable/Views/Auth', 'resources/views/auth');
-        $this->filesystem->copyDirectory('vendor/sectheater/laravel-jarivs/src/Http/Requests', 'app/Http/Requests');
-        $this->filesystem->copyDirectory('vendor/sectheater/laravel-jarivs/src/Http/Rules', 'app/Rules');
+        $this->filesystem->copyDirectory('vendor/sectheater/laravel-jarvis/src/Http/Requests', 'app/Http/Requests');
+        $this->filesystem->copyDirectory('vendor/sectheater/laravel-jarvis/src/Http/Rules', 'app/Rules');
         $bar = $this->output->createProgressBar(8);
         $bar->setFormat('[<fg=magenta>%bar%</>]');
 
