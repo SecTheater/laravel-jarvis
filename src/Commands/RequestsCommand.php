aad1fa40b9dde16655e8981c663ff79db24ca71f
Security Theater <moeteam2016@gmail.com>
2018-03-12 05:16:49 +0200

    no such file or directory fixed

--- src/Commands/RequestsCommand.php
+++ src/Commands/RequestsCommand.php
@@ -40,15 +40,15 @@ class RequestsCommand extends Command
     public function handle()
     {
         $files = $this->filesystem->files(base_path('vendor/sectheater/laravel-jarvis/src/Http/Requests'));
-        $destination_namespace = trim(str_replace(['\\', '/'], DIRECTORY_SEPARATOR, $this->argument('namespace') ?? '\\app\\Http\\Requests'), '/');
+        $destination_namespace = trim(str_replace(['\\', '/'], DIRECTORY_SEPARATOR, $this->argument('namespace') ?? 'app\\Http\\Requests'), '/');
         $destination_namespace = starts_with($destination_namespace, 'App') ? lcfirst($destination_namespace) : $destination_namespace;
         if (str_contains($destination_namespace, 'app')) {
             if (!$this->filesystem->isDirectory($destination_namespace)) {
-                $this->filesystem->makeDirectory($destination_namespace);
+                $this->filesystem->makeDirectory($destination_namespace,0755,false,true);
             }
         } else {
             if (!$this->filesystem->isDirectory(app_path($destination_namespace))) {
-                $this->filesystem->makeDirectory(app_path($destination_namespace));
+                $this->filesystem->makeDirectory(app_path($destination_namespace),0755,false,true);
             }
         }
         foreach ($files as $file) {
