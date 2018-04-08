c0ba680fb35e82a7903dbf9bd80d8db8181296cb
Security Theater <moeteam2016@gmail.com>
2018-04-08 13:15:08 +0200

    Routes option updated

--- src/Commands/InstallCommand.php
+++ src/Commands/InstallCommand.php
@@ -78,16 +78,7 @@ class InstallCommand extends Command
                 }
             }
         }
-        $routes_contents = $this->filesystem->get(base_path('routes/web.php'));
-        if (false === strpos($routes_contents, 'Jarvis::Routes()')) {
-            if (strpos($routes_contents, 'Auth::routes()')) {
-                str_replace('Auth::routes()', '', $routes_contents);
-            }
-            $this->filesystem->append(
-                base_path('routes/web.php'),
-                "\n\n Jarvis::Routes();"
-            );
-        }
+       
         $this->call('vendor:publish', ['--provider' => JarvisServiceProvider::class]);
         $os = (substr(php_uname('a'), 0, 3));
         $name = (strtoupper($os) !== 'WIN') ? posix_getpwuid(posix_geteuid())['name'] : null;
@@ -98,10 +89,16 @@ class InstallCommand extends Command
             $bar = $this->output->createProgressBar(count(config('jarvis')));
             $bar->setFormat('[<fg=magenta>%bar%</>]');
             if ($this->confirm('Will You use our default routes ?')) {
-                config(['jarvis.routes.register' => true]);
-            } else {
-                config(['jarvis.routes.register' => false]);
-            }
+                $routes_contents = $this->filesystem->get(base_path('routes/web.php'));
+                $routeServiceProviderContent =  $this->filesystem->get(app_path('Providers/RouteServiceProvider.php'));
+                if (false === strpos($routeServiceProviderContent, 'Jarvis::routes();')) {
+                    if (strpos($routes_contents, 'Auth::routes();')) {
+                        str_replace('Auth::routes();', '', $routes_contents);
+                    }
+                    $routeServiceProviderContent = str_replace("parent::boot();","parent::boot(); \n \t \t \\Jarvis::routes();",$routeServiceProviderContent);
+                    $this->filesystem->put(app_path('Providers/RouteServiceProvider.php'), $routeServiceProviderContent);
+                }
+            } 
             $bar->advance();
 
             if ($this->confirm('Does Your Application contain of comments system ?')) {
