7b5bc04551d3f3019a7bf04565f1cebb9b5c70e4
Security Theater <moeteam2016@gmail.com>
2018-04-21 19:53:20 +0200

    Update RolesSeeder.php

--- src/publishable/Database/Seeders/RolesSeeder.php
+++ src/publishable/Database/Seeders/RolesSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Role;
 
 class RolesSeeder extends Seeder
 {
@@ -11,10 +12,86 @@ class RolesSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('roles')->insert([
-                ['slug' => 'admin', 'name' => 'Administrator', 'permissions' => '{"admin.posts.show":true,"admin.posts.edit":true,"admin.posts.delete":true,"admin.posts.create":true,"admin.posts.approve":true,"admin.tags.create":true,"admin.tags.edit":true,"admin.tags.delete":true,"admin.tags.show":true,"admin.comments.show":true,"admin.comments.create":true,"admin.comments.edit":true,"admin.comments.delete":true,"admin.comments.approve":true,"admin.replies.create":true,"admin.replies.edit":true,"admin.replies.delete":true,"admin.replies.approve":true,"admin.replies.show":true,"admin.likes.show":true,"admin.likes.edit":true,"admin.likes.create":true,"admin.likes.delete":true,"admin.users.upgrade":true,"admin.users.downgrade":true,"admin.tasks.approve":true}'],
-                ['slug' => 'moderator', 'name' => 'moderator', 'permissions' => '{"moderator.posts.show":true,"moderator.posts.edit":true,"moderator.posts.delete":true,"moderator.posts.create":true,"moderator.posts.approve":false,"moderator.tags.create":true,"moderator.tags.edit":true,"moderator.tags.delete":true,"moderator.tags.show":true,"moderator.comments.show":true,"moderator.comments.create":true,"moderator.comments.edit":true,"moderator.comments.delete":true,"moderator.comments.approve":false,"moderator.replies.create":true,"moderator.replies.edit":true,"moderator.replies.delete":true,"moderator.replies.approve":false,"moderator.replies.show":true,"moderator.likes.show":true,"moderator.likes.edit":true,"moderator.likes.create":true,"moderator.likes.delete":true,"moderator.users.upgrade":false,"moderator.users.downgrade":false}'],
-                ['slug' => 'user', 'name' => 'Normal User', 'permissions' => '{"user.posts.show":true,"user.posts.edit":false,"user.posts.delete":false,"user.posts.create":false,"user.posts.approve":false,"user.tags.create":false,"user.tags.edit":false,"user.tags.delete":false,"user.tags.show":true,"user.comments.show":true,"user.comments.create":true,"user.comments.edit":true,"user.comments.delete":true,"user.comments.approve":false,"user.replies.create":true,"user.replies.edit":true,"user.replies.delete":true,"user.replies.approve":false,"user.replies.show":true,"user.likes.show":true,"user.likes.edit":true,"user.likes.create":true,"user.likes.delete":true}'],
-            ]);
+        $admin = Role::create([
+            'slug' => 'admin',
+            'name' => 'Administrator',
+            'permissions' => [
+                'view-post' => true,
+                'update-post' => true,
+                'delete-post' => true,
+                'create-post' => true,
+                'approve-post' => true,
+                'create-tag' => true,
+                'update-tag' => true,
+                'delete-tag' => true,
+                'view-tag' => true,
+                'view-comment' => true,
+                'update-comment' => true,
+                'delete-comment' => true,
+                'create-comment' => true,
+                'approve-comment' => true,
+                'view-reply' => true,
+                'update-reply' => true,
+                'delete-reply' => true,
+                'create-reply' => true,
+                'approve-reply' => true,
+                'upgrade-user' => true,
+                'downgrade-user' => true,
+            ]
+        ]);
+        $moderator = Role::create([
+            'slug' => 'moderator',
+            'name' => 'Moderator',
+            'permissions' => [
+                'create-post' => true,
+                'view-post' => true,
+                'delete-post' => true,
+                'update-post' => true,
+                'approve-post' => false,
+                'create-tag' => true,
+                'update-tag' => true,
+                'view-tag' => true,
+                'delete-tag' => true,
+                'create-comment' => true,
+                'update-comment' => true,
+                'delete-comment' => true,
+                'view-comment' => true,
+                'approve-comment' => true,
+                'create-reply' => true,
+                'update-reply' => true,
+                'delete-reply' => true,
+                'view-reply' => true,
+                'approve-reply' => true,
+                'upgrade-user' => false,
+                'downgrade-user' => false
+            ]
+        ]);
+        $user = Role::create([
+            'name' => 'Normal User',
+            'slug' => 'user',
+            'permissions' => [
+                'create-post' => false,
+                'view-post' => true,
+                'update-post' => false,
+                'delete-post' => false,
+                'approve-post' => false,
+                'create-tag' => false,
+                'view-tag' => false,
+                'update-tag' => false,
+                'delete-tag' => false,
+                'create-comment' => true,
+                'update-comment' => true,
+                'view-comment' => true,
+                'delete-comment' => true,
+                'approve-comment' => false,
+                'create-reply' => true,
+                'update-reply' => true,
+                'view-reply' => true,
+                'delete-reply' => true,
+                'approve-reply' => false,
+                'upgrade-user' => false,
+                'downgrade-user' => false
+            ],
+        ]);
     }
 }
