f441c10c88ae26335d4029e74a6dd6be2cfb8917
Security Theater <moeteam2016@gmail.com>
2018-04-21 20:15:51 +0200

    Update RoleObserver.php

--- src/Observers/RoleObserver.php
+++ src/Observers/RoleObserver.php
@@ -6,21 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class RoleObserver
 {
-    public function retrieved(Model $role)
-    {
-        $role->permissions = json_decode($role->permissions, true);
-    }
 
     public function creating(Model $role)
     {
-        $role->permissions = json_encode($role->permissions);
         $role->created_at = date('Y-m-d H:i:s');
         $role->updated_at = null;
     }
 
     public function updating(Model $role)
     {
-        $role->permissions = json_encode($role->permissions);
         $role->updated_at = date('Y-m-d H:i:s');
     }
 
