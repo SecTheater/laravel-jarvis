d3532ecf2dc7f1128c3cfbeefdfcd5b40377759f
Security Theater <moeteam2016@gmail.com>
2018-05-22 19:43:30 +0200

    Update JarvisServiceProvider.php

--- src/Providers/JarvisServiceProvider.php
+++ src/Providers/JarvisServiceProvider.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace SecTheater\Jarvis\Providers;
-
+use Illuminate\Support\HtmlString;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Filesystem\Filesystem;
 use Illuminate\Support\Facades\Blade;
@@ -24,220 +24,166 @@ use SecTheater\Jarvis\Role\EloquentRole;
 use SecTheater\Jarvis\Role\RoleRepository;
 use SecTheater\Jarvis\Tag\EloquentTag;
 use SecTheater\Jarvis\Tag\TagRepository;
+use SecTheater\Jarvis\User\EloquentUser;
 use SecTheater\Jarvis\User\UserRepository;
 
-class JarvisServiceProvider extends ServiceProvider
-{
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
-    public function boot(Filesystem $filesystem)
-    {
-        if (config('jarvis.observers.register')) {
-            $this->registerObservers();
-        }
-    }
+class JarvisServiceProvider extends ServiceProvider {
+	/**
+	 * Bootstrap any application services.
+	 *
+	 * @return void
+	 */
+	public function boot(Filesystem $filesystem) {
+		if (config('jarvis.observers.register')) {
+			$this->registerObservers();
+		}
+	}
 
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        if ($this->app->runningInConsole()) {
-            $this->registerPublishables();
-            $this->registerConsoleCommands();
-        }
-        $this->registerHelpers();
-        $this->registerBlades();
-        if (config('jarvis.models.package')) {
-            $this->registerBindings();
-        }
-        Schema::defaultStringLength(191);
-    }
+	/**
+	 * Register any application services.
+	 *
+	 * @return void
+	 */
+	public function register() {
+		if ($this->app->runningInConsole()) {
+			$this->registerPublishables();
+			$this->registerConsoleCommands();
+		}
+		$this->registerHelpers();
+		$this->registerBlades();
+		$this->registerBindings();
+		Schema::defaultStringLength(191);
+	}
 
-    private function registerHelpers()
-    {
-        require_once __DIR__.'/../Helpers/Helpers.php';
-    }
+	private function registerHelpers() {
+		require_once __DIR__ . '/../Helpers/Helpers.php';
+	}
 
-    private function registerPublishables()
-    {
-        $publishablePath = dirname(__DIR__).'/publishable';
-        $publishable = [
-            'migrations' => [
-                "{$publishablePath}/Database/Migrations/" => database_path('migrations'),
-            ],
-            'seeds' => [
-                "{$publishablePath}/Database/Seeders/" => database_path('seeds'),
-            ],
-            'config' => [
-                "{$publishablePath}/config/jarvis.php" => config_path('jarvis.php'),
-            ],
-            'Markdown' => [
-                "{$publishablePath}/Views/Emails/" => resource_path('views/emails'),
-            ],
-            'Emails' => [
-                "{$publishablePath}/Mail" => app_path('Mail'),
-            ],
+	private function registerPublishables() {
+		$publishablePath = dirname(__DIR__) . '/publishable';
+		$publishable = [
+			'migrations' => [
+				"{$publishablePath}/Database/Migrations/" => database_path('migrations'),
+			],
+			'seeds' => [
+				"{$publishablePath}/Database/Seeders/" => database_path('seeds'),
+			],
+			'config' => [
+				"{$publishablePath}/config/jarvis.php" => config_path('jarvis.php'),
+			],
+			'Markdown' => [
+				"{$publishablePath}/Views/Emails/" => resource_path('views/emails'),
+			],
+			'Emails' => [
+				"{$publishablePath}/Mail" => app_path('Mail'),
+			],
 
-        ];
-        foreach ($publishable as $group => $paths) {
-            $this->publishes($paths, $group);
-        }
-    }
+		];
+		foreach ($publishable as $group => $paths) {
+			$this->publishes($paths, $group);
+		}
+	}
 
-    private function registerConsoleCommands()
-    {
-        $this->commands(\SecTheater\Jarvis\Commands\InstallCommand::class);
-        $this->commands(\SecTheater\Jarvis\Commands\ControllersCommand::class);
-        $this->commands(\SecTheater\Jarvis\Commands\AuthCommand::class);
-        $this->commands(\SecTheater\Jarvis\Commands\AssignUserCommand::class);
-        $this->commands(\SecTheater\Jarvis\Commands\CustomValidationCommand::class);
-        $this->commands(\SecTheater\Jarvis\Commands\RequestsCommand::class);
-        $this->commands(\SecTheater\Jarvis\Commands\ObserversCommand::class);
-        $this->commands(\SecTheater\Jarvis\Commands\RegisterAuthorizationCommand::class);
-        $this->commands(\SecTheater\Jarvis\Commands\SeederCommand::class);
-        //  $this->commands(Commands\CreatePermissionCommand::class);
-    }
+	private function registerConsoleCommands() {
+		$this->commands(\SecTheater\Jarvis\Commands\InstallCommand::class);
+		$this->commands(\SecTheater\Jarvis\Commands\ControllersCommand::class);
+		$this->commands(\SecTheater\Jarvis\Commands\AuthCommand::class);
+		$this->commands(\SecTheater\Jarvis\Commands\AssignUserCommand::class);
+		$this->commands(\SecTheater\Jarvis\Commands\CustomValidationCommand::class);
+		$this->commands(\SecTheater\Jarvis\Commands\RequestsCommand::class);
+		$this->commands(\SecTheater\Jarvis\Commands\ObserversCommand::class);
+		$this->commands(\SecTheater\Jarvis\Commands\RegisterAuthorizationCommand::class);
+		$this->commands(\SecTheater\Jarvis\Commands\SeederCommand::class);
+		//  $this->commands(Commands\CreatePermissionCommand::class);
+	}
 
-    private function registerObservers()
-    {
-        foreach ($this->models as $key => $value) {
-            $value::observe("\SecTheater\Jarvis\Observers\\".ucfirst($key).'Observer');
-        }
-    }
+	private function registerObservers() {
+		foreach ($this->models as $key => $value) {
+			$value::observe("\SecTheater\Jarvis\Observers\\" . ucfirst($key) . 'Observer');
+		}
+	}
 
-    protected function registerBindings()
-    {
-        $this->app->singleton('Jarvis', function () {
-            // foreach ($this->models as $key => $value) {
-            // 	$class = ucfirst($key) . 'Repository';
-            // 	dd($class,$value, new $class);
-            // 	$$key = new $class(new $value);
-            // }
-            $user = new UserRepository(new $this->models['user']());
-            $activation = new ActivationRepository(new $this->models['activation']());
-            $role = new RoleRepository(new $this->models['role']());
-            $post = new PostRepository(new $this->models['post']());
-            $comment = new CommentRepository(new $this->models['comment']());
-            $reply = new ReplyRepository(new $this->models['reply']());
-            $reminder = new ReminderRepository(new $this->models['reminder']());
-            $like = new LikeRepository(new $this->models['like']());
-            $tag = new TagRepository(new $this->models['tag']());
+	protected function registerBindings() {
+		$this->app->singleton('Jarvis', function () {
+			foreach ($this->models as $key => $value) {
+				$class = '\SecTheater\Jarvis\\'.ucfirst($key) . '\\'. ucfirst($key).'Repository';
+				$$key = new $class(new $value);
+			}
+			return new \SecTheater\Jarvis\Jarvis(
+				$user,
+				$activation,
+				$role,
+				$post,
+				$comment,
+				$reply,
+				$reminder,
+				$like,
+				$tag
+			);
+		});
+		foreach ($this->models as $key => $value) {
+			$class = '\SecTheater\Jarvis\\'.ucfirst($key) . '\\'. ucfirst($key).'Repository';
+			$model = new $class(new $value);
+			if($key == 'user' || $key == 'role' || config('jarvis.'. str_plural($key) . '.register')){
+				$this->app->singleton(class_basename($class),function() use($model){
+					return $model;
+				});
+			}
+		}
+	}
 
-            return new \SecTheater\Jarvis\Jarvis(
-                $user,
-                $activation,
-                $role,
-                $post,
-                $comment,
-                $reply,
-                $reminder,
-                $like,
-                $tag
-            );
-        });
-        if (config('jarvis.posts.register')) {
-            $this->app->singleton('PostRepository', function () {
-                return new PostRepository(new EloquentPost());
-            });
-        }
-        if (config('jarvis.comments.register')) {
-            $this->app->singleton('CommentRepository', function () {
-                return new CommentRepository(new EloquentComment());
-            });
-        }
+	private function registerBlades() {
+		Blade::if ('hasRole', function (string $role, Model $user = null) {
+			if (!isset($user)) {
+				$user = auth()->user();
+			}
 
-        if (config('jarvis.activation.register')) {
-            $this->app->singleton('ActivationRepository', function () {
-                return new ActivationRepository(new EloquentActivation());
-            });
-        }
-        if (config('jarvis.replies.register')) {
-            $this->app->singleton('ReplyRepository', function () {
-                return new ReplyRepository(new EloquentReply());
-            });
-        }
-        $this->app->singleton('ReminderRepository', function () {
-            return new ReminderRepository(new EloquentReminder());
-        });
-        if (config('jarvis.likes.register')) {
-            $this->app->singleton('LikeRepository', function () {
-                return new LikeRepository(new EloquentLike());
-            });
-        }
-        $this->app->singleton('RoleRepository', function () {
-            return new RoleRepository(new EloquentRole());
-        });
-        if (config('jarvis.tags.register')) {
-            $this->app->singleton('TagRepository', function () {
-                return new TagRepository(new EloquentTag());
-            });
-        }
-        $this->app->singleton('UserRepository', function () {
-            return new UserRepository(new \App\User());
-        });
-    }
+			return auth()->check() && $user->hasRole($role);
+		});
+		Blade::if ('hasAnyRole', function (array $role, Model $user = null) {
+			if (!isset($user)) {
+				$user = auth()->user();
+			}
 
-    private function registerBlades()
-    {
-        Blade::if('hasRole', function (string $role, Model $user = null) {
-            if (!isset($user)) {
-                $user = auth()->user();
-            }
+			return auth()->check() && $user->hasAnyRole($role);
+		});
+		Blade::if ('equals', function (Model $user) {
+			return auth()->check() && auth()->user()->id === $user->id;
+		});
+		Blade::if ('role', function (string $slug, Model $user = null) {
+			if (!isset($user)) {
+				$user = auth()->user();
+			}
 
-            return auth()->check() && $user->hasRole($role);
-        });
-        Blade::if('hasAnyRole', function (array $role, Model $user = null) {
-            if (!isset($user)) {
-                $user = auth()->user();
-            }
+			return $user->roles->first()->slug === $slug;
+		});
+		Blade::if ('hasAllRole', function (array $role, Model $user = null) {
+			if (!isset($user)) {
+				$user = auth()->user();
+			}
 
-            return auth()->check() && $user->hasAnyRole($role);
-        });
-        Blade::if('equals', function (Model $user) {
-            return auth()->check() && auth()->user()->id === $user->id;
-        });
-        Blade::if('role', function (string $slug, Model $user = null) {
-            if (!isset($user)) {
-                $user = auth()->user();
-            }
+			return auth()->check() && $user->hasAllRole($role);
+		});
+		Blade::if ('inRole', function (string $slug, Model $user = null) {
+			if (!isset($user)) {
+				$user = auth()->user();
+			}
 
-            return $user->roles->first()->slug === $slug;
-        });
-        Blade::if('hasAllRole', function (array $role, Model $user = null) {
-            if (!isset($user)) {
-                $user = auth()->user();
-            }
+			return auth()->check() && $user->inRole($slug);
+		});
 
-            return auth()->check() && $user->hasAllRole($role);
-        });
-        Blade::if('inRole', function (string $slug, Model $user = null) {
-            if (!isset($user)) {
-                $user = auth()->user();
-            }
+	}
+	public function __get($key){
+		if(count(config('jarvis.models.user'))){
+			foreach(config('jarvis.models.user') as $key => $value){
+				$this->models[$key] = $value;
+			}
+		}
+		foreach (config('jarvis.models.package') as $key => $value) {
+			$this->models[$key]  = $value;
+		}
 
-            return auth()->check() && $user->inRole($slug);
-        });
-    }
+		return $this->models ?? null;
+	}
 
-    public function __get($key)
-    {
-        if (config('jarvis')) {
-            if (count(config('jarvis.models.user'))) {
-                foreach (config('jarvis.models.user') as $key => $value) {
-                    $this->models[$key] = $value;
-                }
-            }
-            foreach (config('jarvis.models.package') as $key => $value) {
-                $this->models[$key] = $value;
-            }
-        }
-
-        return $this->models ?? null;
-    }
 }
