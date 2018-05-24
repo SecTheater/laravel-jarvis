3afed9b2bf784fa487f397723175dc1ae18f4da9
Security Theater <moeteam2016@gmail.com>
2018-05-24 19:56:49 +0000

    Apply fixes from StyleCI

--- src/Model/EloquentModel.php
+++ src/Model/EloquentModel.php
@@ -1,30 +1,39 @@
 <?php
+
 namespace SecTheater\Jarvis\Model;
+
 use Illuminate\Database\Eloquent\Model;
 
-class EloquentModel extends Model {
-	protected $guarded = [];
-	public function __get($name) {
-		if (!property_exists(static::class, $name) && str_contains($name, 'Model')) {
-			$model = str_replace('Model', '', $name);
-			if (model_exists($model)) {
-				$key = 'jarvis.models.user.' . $model;
-				$value = '\\' . config('jarvis.models.namespace') . ucfirst($model);
-				config([$key => $value]);
-				$this->{$name} = $value;
-			} elseif (jarvis_model_exists($model)) {
-				$this->{$name} = config('jarvis.models.package.' . $model);
-			}
+class EloquentModel extends Model
+{
+    protected $guarded = [];
 
-			return $this->{$name};
-		}
-		return parent::__get($name);
-	}
-	public function __set($key, $value) {
-		if ($this->getAttribute($key)) {
-			parent::__set($key, $value);
-			return;
-		}
-		$this->{$key} = $value;
-	}
+    public function __get($name)
+    {
+        if (!property_exists(static::class, $name) && str_contains($name, 'Model')) {
+            $model = str_replace('Model', '', $name);
+            if (model_exists($model)) {
+                $key = 'jarvis.models.user.'.$model;
+                $value = '\\'.config('jarvis.models.namespace').ucfirst($model);
+                config([$key => $value]);
+                $this->{$name} = $value;
+            } elseif (jarvis_model_exists($model)) {
+                $this->{$name} = config('jarvis.models.package.'.$model);
+            }
+
+            return $this->{$name};
+        }
+
+        return parent::__get($name);
+    }
+
+    public function __set($key, $value)
+    {
+        if ($this->getAttribute($key)) {
+            parent::__set($key, $value);
+
+            return;
+        }
+        $this->{$key} = $value;
+    }
 }
