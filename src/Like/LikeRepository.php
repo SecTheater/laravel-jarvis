47ae544cb82caa4f246c9fd581bdc63a74f57694
Security Theater <moeteam2016@gmail.com>
2018-05-29 00:49:35 +0200

    Update LikeRepository.php

--- src/Like/LikeRepository.php
+++ src/Like/LikeRepository.php
@@ -4,74 +4,56 @@ namespace SecTheater\Jarvis\Like;
 
 use SecTheater\Jarvis\Repositories\Repository;
 
-class LikeRepository extends Repository implements LikeInterface
-{
-    protected $model;
+class LikeRepository extends Repository implements LikeInterface {
+	protected $model;
 
-    public function __construct(EloquentLike $model)
-    {
-        $this->model = $model;
-    }
+	public function __construct(EloquentLike $model) {
+		$this->model = $model;
+	}
 
-    public function likeStatus($type, int $user_id = null)
-    {
-        $existence = $this->findBy(['user_id' => $user_id ?? user()->id, 'likable_type' => class_basename($type), 'likable_id' => $type->id]);
-        if (count($existence) > 0) {
-            return $existence->first()->like_status;
-        }
-    }
+	public function likeStatus($type, int $user_id = null) {
+		return (bool) $type->likes()->where('user_id',$user_id ?? user()->id )->first()->status;
+	}
 
-    public function likeCounter($type, bool $status = true): int
-    {
-        return $this->model->where(['likable_id' => $type->id, 'likable_type' => class_basename($type), 'like_status' => $status])->count();
-    }
+	public function likeCounter($type, bool $status = true): int {
+		return $type->likes()->whereStatus($status)->count();
+	}
 
-    public function like($type)
-    {
-        if ($this->exists(['user_id' => user()->id, 'likable_id' => $type->id, 'likable_type' => class_basename($type)])) {
-            $record = $this->model->where(['user_id' => user()->id, 'likable_id' => $type->id, 'likable_type' => class_basename($type)])->first();
-            // type is already liked, and failed to like it again.
-            if ($record->like_status) {
-                return false;
-            }
+	public function like($type) {
+		if ($type->likes()->exists()) {
+			// already liked.
+			if($type->likes->first()->status){
+				return false;
+			}
+			$type->likes()->update(['status' => true]);
+			return $type;
+		}
+		$like = model('Like');
+	    $like->status = true;
+	    $like->user_id = user()->id;
+	    $type->likes()->save($like);
+	    return $type;
+	}
 
-            return (bool) $record->update(['like_status' => true]);
-        }
+	public function removeLike($type) {
+		$type->likes()->delete();
+		return $type;
+	}
 
-        return $this->create([
-            'user_id'      => user()->id,
-            'likable_type' => class_basename($type),
-            'likable_id'   => $type->id,
-            'like_status'  => true,
-        ]);
-    }
+	public function dislike($type) {
+		if($type->likes()->exists()){
+			// already disliked.
+			if(!$type->likes->first()->status){
+				return false;
+			}
+			$type->likes()->update(['status' => false]);
+			return $type;
+		}
 
-    public function removeLike($type)
-    {
-        if ($this->exists(['user_id' => user()->id, 'likable_id' => $type->id, 'likable_type' => class_basename($type)])) {
-            return (bool) $this->model->where(['user_id' => user()->id, 'likable_id' => $type->id, 'likable_type' => class_basename($type)])->delete();
-        }
-
-        return false;
-    }
-
-    public function dislike($type)
-    {
-        if ($this->exists(['user_id' => user()->id, 'likable_id' => $type->id, 'likable_type' => class_basename($type)])) {
-            $record = $this->model->where(['user_id' => user()->id, 'likable_id' => $type->id, 'likable_type' => class_basename($type)])->first();
-            // type is already disliked, and failed to like it again.
-            if (!$record->like_status) {
-                return false;
-            }
-
-            return (bool) $record->update(['like_status' => false]);
-        }
-
-        return $this->create([
-            'user_id'      => user()->id,
-            'likable_type' => class_basename($type),
-            'likable_id'   => $type->id,
-            'like_status'  => false,
-        ]);
-    }
+		$like = model('Like');
+	    $like->status = false;
+	    $like->user_id = user()->id;
+	    $type->likes()->save($like);
+	    return $type;
+	}
 }
