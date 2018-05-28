48b003e72db30bff8433825d765a1eb88871dcb7
Security Theater <moeteam2016@gmail.com>
2018-05-28 23:19:07 +0000

    Apply fixes from StyleCI

--- src/Like/EloquentLike.php
+++ src/Like/EloquentLike.php
@@ -1,26 +1,33 @@
 <?php
 
 namespace SecTheater\Jarvis\Like;
+
 use Illuminate\Database\Eloquent\Relations\Relation;
 use SecTheater\Jarvis\Model\EloquentModel;
 
-class EloquentLike extends EloquentModel {
-	protected $casts = [
-		'like_status' => 'boolean',
-	];
-	protected $table = 'likes';
- 	public function __construct(){
+class EloquentLike extends EloquentModel
+{
+    protected $casts = [
+        'like_status' => 'boolean',
+    ];
+    protected $table = 'likes';
+
+    public function __construct()
+    {
         Relation::morphMap([
-            'Post' => $this->postModel,
+            'Post'    => $this->postModel,
             'Comment' => $this->commentModel,
-            'Reply' => $this->replyModel
+            'Reply'   => $this->replyModel,
         ]);
     }
+
     public function likable()
     {
         return $this->morphTo();
     }
-	public function user() {
-		return $this->belongsTo($this->userModel, 'user_id');
-	}
+
+    public function user()
+    {
+        return $this->belongsTo($this->userModel, 'user_id');
+    }
 }
