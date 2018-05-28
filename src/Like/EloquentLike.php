66520ff78ae8e47214dd06ae4172535f93cfcf15
Security Theater <moeteam2016@gmail.com>
2018-05-29 01:19:02 +0200

    Update EloquentLike.php

--- src/Like/EloquentLike.php
+++ src/Like/EloquentLike.php
@@ -1,43 +1,26 @@
 <?php
 
 namespace SecTheater\Jarvis\Like;
-
 use Illuminate\Database\Eloquent\Relations\Relation;
 use SecTheater\Jarvis\Model\EloquentModel;
 
-class EloquentLike extends EloquentModel
-{
-    protected $casts = [
-        'like_status' => 'boolean',
-    ];
-    protected $table = 'likes';
-
-    public function __construct()
-    {
+class EloquentLike extends EloquentModel {
+	protected $casts = [
+		'like_status' => 'boolean',
+	];
+	protected $table = 'likes';
+ 	public function __construct(){
         Relation::morphMap([
-            'Post'    => $this->postModel,
+            'Post' => $this->postModel,
             'Comment' => $this->commentModel,
-            'Reply'   => $this->replyModel,
+            'Reply' => $this->replyModel
         ]);
     }
-
     public function likable()
     {
         return $this->morphTo();
     }
-
-    public function comments()
-    {
-        return $this->morphedByMany($this->commentModel, 'likable', 'likables', 'like_id', 'likable_id');
-    }
-
-    public function replies()
-    {
-        return $this->morphedByMany($this->replyModel, 'likable', 'likables', 'like_id', 'likable_id');
-    }
-
-    public function user()
-    {
-        return $this->belongsTo($this->userModel, 'user_id');
-    }
+	public function user() {
+		return $this->belongsTo($this->userModel, 'user_id');
+	}
 }
